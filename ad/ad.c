9b6852b021942b2076710311c0397ad29125d167
WangsirCode <bryantwangsir@qq.com>
2018-04-04 18:48:03 +0800

    eidt ad

--- ad/ad.c
+++ ad/ad.c
@@ -1,26 +1,6 @@
 ﻿#include "ad.h"
 /*
  * 函数说明：
- * 通过给定的输入信号频率和FFT点数计算并设置定时器的预置数
- * 参数说明：
- * Freq为输入信号的频率，单位为赫兹
- * pointN为fft点数
- * BASE为相应定时器基地址
- */
-void SetFreq(int Freq,int pointN,int BASE)
-{
-    int preData = 1;//待求预置数
-
-    preData = 50000000/(Freq * pointN);
-    //修改预置数寄存器的值
-    IOWR_ALTERA_AVALON_TIMER_PERIODL(BASE, 0xFFFF&preData);
-    IOWR_ALTERA_AVALON_TIMER_PERIODH(BASE, (0xFFFF0000&preData)>>4);
-
-    IOWR_ALTERA_AVALON_TIMER_CONTROL(BASE, 0x07);
-}
-
-/*
- * 函数说明：
  * 调用函数可读取AD转换结果（二补数），并返回其真实值
  * 参数说明：
  * BASE为相应AD模块数据位链接的并行IO口的基地址
--- ad/ad.h
+++ ad/ad.h
@@ -8,9 +8,6 @@
 #ifndef AD_H_
 #define AD_H_
 #include "altera_avalon_pio_regs.h"
-#include "altera_avalon_timer.h"
-#include "altera_avalon_timer_regs.h"
-void SetFreq(int Freq,int pointN,int BASE);
 float ReadAdRes(int BASE);
 void parseAdData(float *input, unsigned char * source);
 #endif /* AD_H_ */
